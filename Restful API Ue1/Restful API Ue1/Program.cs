9c8b9f1cf62feb3180f8a68e52ca18b7b3f07156
Abouzaid Tarek <if18b119@technikum-wien.at>
2020-11-02 14:13:13 +0100

    C# .Core Projekt added

---
+++ Restful API Ue1/Restful API Ue1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Restful_API_Ue1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

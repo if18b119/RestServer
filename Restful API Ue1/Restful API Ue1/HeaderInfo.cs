99e39407e1a5ea27bdb1c8d1ff0bed93bb472687
Abouzaid Tarek <if18b119@technikum-wien.at>
2020-11-02 14:51:19 +0100

    Added Class HeaderInfo

---
+++ Restful API Ue1/Restful API Ue1/HeaderInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restful_API_Ue1
+{
+    class HeaderInfo
+    {
+        public string key { get; private set; }
+        public string value { get; private set; }
+        public HeaderInfo(string x, string y)
+        {
+            key = x;
+            value = y;
+        }
+    }
+}
--- Restful API Ue1/Restful API Ue1/RequestKontext.cs
+++ Restful API Ue1/Restful API Ue1/RequestKontext.cs
@@ -6,5 +6,12 @@ namespace Restful_API_Ue1
 {
     class RequestKontext
     {
+        public String Type { get; private set; }
+        public String Options { get; private set; }
+        public String Protocol { get; private set; }
+        public String Body { get; private set; }
+
+        public static List<HeaderInfo> HeaderInformation = new List<HeaderInfo>();
+
     }
 }
